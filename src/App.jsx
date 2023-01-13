0b59560361f134fd00ae5fd2903e86c86251ee46
Nelson Onuegbu <95982650+nelsonleone@users.noreply.github.com>
2023-01-13 21:30:15 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,17 +26,30 @@ function App() {
 
 
   // handling the chosen tags , and rendering there contents
-  function renderChosenTag(){
+ function renderChosenTag(tag){
     const newArray = listData.filter((data,index) => {
-       const tag = chosenTagsArray[index];
+      if(data.role === tag || data.role === tagsArray[index]){
+        return data
+      }
+     else if(data.level === tag ||data.level === tagsArray[index]){
+        return data
+      }
+     else if(data.languages.filter(language => language === tag || language === tagsArray[index])[0]){
+        return data
+      }
+     else if(data.tools.filter(tool => tool === tag || tool === tagsArray[index])[0]){
+        return data
+      }
+
+     else if(tag === data.role && 
+        tag === data.level && 
+        tag === data.languages.filter(language => language === tag)[0] &&
       if(tag === data.role || 
         tag === data.level || 
         tag === data.languages.filter(language => language === tag)[0] ||
         tag ==  data.tools.filter(tool => tool === tag)[0]
         ){
-         return data;
-      }else{
-        console.log("no",tag)
+        return data;
       }
     })
     setJobsData(newArray)
